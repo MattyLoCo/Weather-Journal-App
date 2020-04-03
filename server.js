f4b4e6e39b50dcfe24174de3fd5021fb9cf5ce0b
Matthew Lopez <lopezmatt6891@gmail.com>
2020-04-03 12:06:00 -0600

    Install cors

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const cors = require('cors');
